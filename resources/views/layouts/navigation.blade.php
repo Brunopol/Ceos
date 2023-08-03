1d31a80f1424de77854e143e9f411172b3d11269
Bruno <bettiol.2023@gmail.com>
2023-08-03 16:04:00 -0300

    Add: logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ asset('img/logo-icon-no-background.png') }}" class="logo-img" width="40" height="40" />
                     </a>
                 </div>
 
