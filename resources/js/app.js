316f35f705cc2869d42f32f5fc720a54f8ccc4ca
Bruno <bettiol.2023@gmail.com>
2023-08-06 12:42:01 -0300

    Add: DataTable support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,32 @@
 import './bootstrap';
 
+import jQuery from 'jquery';
+
+window.$ = jQuery;
+
+import pdfmake from 'pdfmake';
+
+import DataTable from 'datatables.net-dt';
+
+import 'datatables.net-buttons-dt';
+
+import 'datatables.net-buttons/js/buttons.html5.mjs';
+
+import 'datatables.net-fixedheader-dt';
+
+import 'datatables.net-responsive-dt';
+
+import 'datatables.net-searchbuilder-dt';
+
+import 'datatables.net-searchpanes-dt';
+
+let table = new DataTable('#myTable', {
+    // config options...
+});
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Users') }}
@@ -9,9 +10,40 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Você está na tela de usuários!") }}
+                    <table id="myTable" class="display">
+                        <thead>
+                            <tr>
+                                <th>Column 1</th>
+                                <th>Column 2</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Row 1 Data 1</td>
+                                <td>Row 1 Data 2</td>
+                            </tr>
+                            <tr>
+                                <td>Row 2 Data 1</td>
+                                <td>Row 2 Data 2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <button class="btn btn-success">Click Me</button>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
+
+   <script type="module">
+        $("button").click(function(){
+            alert("Thanks");
+        });
+
+        $(document).ready(function() {
+        $('#myTable').DataTable();
+         });
+    </script>
+
