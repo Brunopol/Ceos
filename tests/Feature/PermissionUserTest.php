402daac382865425f8dfc0da3e4b79a334c89748
Bruno <bettiol.2023@gmail.com>
2023-08-08 21:21:01 -0300

    Add: permissions

--- tests/Feature/PermissionUserTest.php
+++ tests/Feature/PermissionUserTest.php
@@ -5,14 +5,18 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Testing\Assert;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertTrue;
 
 class PermissionUserTest extends TestCase
 {
 
     public function testShouldBeAbleToGivePermissionToAnUser()
     {
-        $user = User::factory()->create();
+        $user = User::factory()->createOne();
 
         $user->givePermissionTo('Modulo-Users');
 
@@ -20,6 +24,46 @@ class PermissionUserTest extends TestCase
         $this->assertDatabaseHas('permissions', [
             'permission' => 'Modulo-Users',
         ]);
+
+    }
+
+    public function testShouldBeAbleToAuthorizeAccessToARouteBasedOnThePermission()
+    {
+
+        Route::get('permission-route-testing', function() {
+            return 'test';
+        })->middleware('permission:Modulo-Users');
+
+        /** @var User $user */
+
+        $user = User::factory()->createOne();
+
+        $this->actingAs($user)->get('permission-route-testing')
+        ->assertForbidden();
+
+        $user->givePermissionTo('Modulo-Users');
+
+        $this->actingAs($user)->get('permission-route-testing')
+        ->assertSuccessful();
+
+    }
+
+    public function testandoValores() 
+    {
+
+        /** @var User $user */
+
+        $user = User::factory()->createOne();
+
+        $user->givePermissionTo('Modulo-Users');
+
+        $user->givePermissionTo('Modulo2-Users');
+
+        $user->givePermissionTo('Modulo3-Users');
+
+        assertTrue($user->hasPermissionTo('Modulo-Users'));
+        assertTrue($user->hasPermissionTo('Modulo2-Users'));
+        assertTrue($user->hasPermissionTo('Modulo3-Users'));
 
     }
 }
