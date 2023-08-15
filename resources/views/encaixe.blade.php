5947d49dfd9f57059f0b7a11a0f2493e5c4a3985
Bruno <bettiol.2023@gmail.com>
2023-08-15 20:15:15 -0300

    encaixe-front-end hotfix

--- resources/views/encaixe.blade.php
+++ resources/views/encaixe.blade.php
@@ -20,31 +20,162 @@
                         <thead>
                             <tr>
                                 <th>ref</th>
-                                <th>tecido</th>
-                                <th>largura</th>
-                                <th>qtd_pecas</th>
-                                <th>consumo</th>
+                                <th>editar</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($encaixes as $encaixe)
                             <tr>
-                                <td>{{ $encaixe->ref }}</td>
-                                <td>{{ $encaixe->tecido }}</td>
-                                <td>{{ $encaixe->largura }}</td>
-                                <td>{{ $encaixe->qtd_pecas }}</td>
-                                <td>{{ $encaixe->consumo }}</td>
+                                <td>{{ $encaixe->referencia }}</td>
+                                <td>
+                                    <button class="bg-blue-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" data-url="" onclick="toggleModal('modal-id', $(this).data('url'))">
+                                        editar
+                                    </button>                                    
+                                </td>
                             </tr>
                             @endforeach
 
                         </tbody>
                     </table>
 
+
+                    <!-- Model -->
+                    <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center" id="modal-id">
+                        <div class="relative w-auto my-6 mx-auto max-w-3xl">
+                            <!-- Content -->
+                            <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                                    <!-- Header -->
+                                    <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                        <h3 class="text-3xl font-semibold">
+                                            Editar Encaixe
+                                        </h3>
+                                    </div>
+                                    <!-- Body -->
+                                   
+                                        <!--Tabs navigation-->
+                                        <ul
+                                        class="mr-4 flex list-none flex-col flex-wrap pl-0"
+                                        role="tablist"
+                                        data-te-nav-ref>
+                                        <li role="presentation" class="flex-grow text-center">
+                                            <a
+                                            href="#tabs-home03"
+                                            class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+                                            data-te-toggle="pill"
+                                            data-te-target="#tabs-home03"
+                                            data-te-nav-active
+                                            role="tab"
+                                            aria-controls="tabs-home03"
+                                            aria-selected="true"
+                                            >Home</a
+                                            >
+                                        </li>
+                                        <li role="presentation" class="flex-grow text-center">
+                                            <a
+                                            href="#tabs-profile03"
+                                            class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+                                            data-te-toggle="pill"
+                                            data-te-target="#tabs-profile03"
+                                            role="tab"
+                                            aria-controls="tabs-profile03"
+                                            aria-selected="false"
+                                            >Profile</a
+                                            >
+                                        </li>
+                                        <li role="presentation" class="flex-grow text-center">
+                                            <a
+                                            href="#tabs-messages03"
+                                            class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[te-nav-active]:border-primary data-[te-nav-active]:text-primary dark:text-neutral-400 dark:hover:bg-transparent dark:data-[te-nav-active]:border-primary-400 dark:data-[te-nav-active]:text-primary-400"
+                                            data-te-toggle="pill"
+                                            data-te-target="#tabs-messages03"
+                                            role="tab"
+                                            aria-controls="tabs-messages03"
+                                            aria-selected="false"
+                                            >Messages</a
+                                            >
+                                        </li>
+                                        <li role="presentation" class="flex-grow text-center">
+                                            <a
+                                            href="#tabs-contact03"
+                                            class="disabled pointer-events-none my-2 block border-x-0 border-b-2 border-t-0 border-transparent bg-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-400 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent dark:text-neutral-600"
+                                            data-te-toggle="pill"
+                                            data-te-target="#tabs-contact03"
+                                            role="tab"
+                                            aria-controls="tabs-contact03"
+                                            aria-selected="false"
+                                            >Contact</a
+                                            >
+                                        </li>
+                                        </ul>
+
+                                        <!--Tabs content-->
+                                        <div class="my-2">
+                                        <div
+                                            class="hidden opacity-100 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                                            id="tabs-home03"
+                                            role="tabpanel"
+                                            aria-labelledby="tabs-home-tab03"
+                                            data-te-tab-active>
+                                            Tab 1 content
+                                        </div>
+                                        <div
+                                            class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                                            id="tabs-profile03"
+                                            role="tabpanel"
+                                            aria-labelledby="tabs-profile-tab03">
+                                            Tab 2 content
+                                        </div>
+                                        <div
+                                            class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                                            id="tabs-messages03"
+                                            role="tabpanel"
+                                            aria-labelledby="tabs-profile-tab03">
+                                            Tab 3 content
+                                        </div>
+                                        <div
+                                            class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[te-tab-active]:block"
+                                            id="tabs-contact03"
+                                            role="tabpanel"
+                                            aria-labelledby="tabs-contact-tab03">
+                                            Tab 4 content
+                                        </div>
+                                        </div>
+
+                                    <!-- Footer -->
+                                    <div id="errorMessage" class="hidden mt-1 bg-red-500 text-white p-1 rounded-b shadow-md items-center border-t border-solid border-slate-200">
+                                        <span>Error, prencha corretamente todos os campos</span>
+                                    </div>
+                                    <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                        <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="closeModal('modal-id')">
+                                            Fechar
+                                        </button>
+                                        <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" onclick="updateUser(event)">
+                                            Salvar
+                                        </button>    
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="modal-id-backdrop"></div>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+
+<script type="text/javascript">
+    function toggleModal(modalID, userURL) {
+        
+        $("#" + modalID).toggleClass("hidden flex");
+        $("#" + modalID + "-backdrop").toggleClass("hidden flex");
+
+    }
+
+    function closeModal(modalID) {
+        $("#" + modalID).toggleClass("hidden flex");
+        $("#" + modalID + "-backdrop").toggleClass("hidden flex");
+    }
+</script>
 
 
 <script type="module">
