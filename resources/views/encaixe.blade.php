7dd6c06bdac1ba7d8b8ed1fccc68e33ec4f42b3b
Bruno <bettiol.2023@gmail.com>
2023-08-17 10:51:38 -0300

    update

--- resources/views/encaixe.blade.php
+++ resources/views/encaixe.blade.php
@@ -28,7 +28,7 @@
                             <tr>
                                 <td>{{ $encaixe->referencia }}</td>
                                 <td>
-                                    <button class="bg-blue-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" data-url="{{ route('encaixes.show', $encaixe->id) }}" onclick="toggleModal('modal-id', $(this).data('url'))">
+                                    <button class="bg-blue-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" data-url="{{ route('encaixes.show', $encaixe->id) }}" data-referencia="{{ $encaixe->referencia }}" onclick="toggleModal('modal-id', $(this).data('url'), $(this).data('referencia'))">
                                         editar
                                     </button>                                    
                                 </td>
@@ -46,7 +46,7 @@
                             <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                                     <!-- Header -->
                                     <div class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                        <h3 class="text-3xl font-semibold">
+                                        <h3 class="text-3xl font-semibold" id="tituloEncaixeRef">
                                             Editar Encaixe
                                         </h3>
                                     </div>
@@ -55,77 +55,19 @@
                                         <div class="flex flex-wrap" id="tabs-id">
                                         <div class="w-full">
                                             <ul class="flex mb-0 list-none flex-wrap pt-3 pb-4 flex-row">
-                                            <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                                                <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-white bg-blue-500" onclick="changeAtiveTab(event,'tab-profile')">
-                                                <i class="fas fa-space-shuttle text-base mr-1"></i>  Profile
-                                                </a>
-                                            </li>
-                                            <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                                                <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-pink-600 bg-white" onclick="changeAtiveTab(event,'tab-settings')">
-                                                <i class="fas fa-cog text-base mr-1"></i>  Settings
-                                                </a>
-                                            </li>
-                                            <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
-                                                <a class="text-xs font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal text-pink-600 bg-white" onclick="changeAtiveTab(event,'tab-options')">
-                                                <i class="fas fa-briefcase text-base mr-1"></i>  Options
-                                                </a>
-                                            </li>
+                                          
                                             </ul>
                                             <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
                                             <div class="px-4 py-5 flex-auto">
                                                 <div class="tab-content tab-space">
-                                                <div class="block" id="tab-profile">
-                                                    <p>
-                                                    Collaboratively administrate empowered markets via
-                                                    plug-and-play networks. Dynamically procrastinate B2C users
-                                                    after installed base benefits.
-                                                    <br />
-                                                    <br />
-                                                    Dramatically visualize customer directed convergence
-                                                    without revolutionary ROI.
-                                                    </p>
-                                                </div>
-                                                <div class="hidden" id="tab-settings">
-                                                    <p>
-                                                    Completely synergize resource taxing relationships via
-                                                    premier niche markets. Professionally cultivate one-to-one
-                                                    customer service with robust ideas.
-                                                    <br />
-                                                    <br />
-                                                    Dynamically innovate resource-leveling customer service for
-                                                    state of the art customer service.
-                                                    </p>
-                                                </div>
-                                                <div class="hidden" id="tab-options">
-                                                    <p>
-                                                    Efficiently unleash cross-media information without
-                                                    cross-media value. Quickly maximize timely deliverables for
-                                                    real-time schemas.
-                                                    <br />
-                                                    <br />
-                                                    Dramatically maintain clicks-and-mortar solutions
-                                                    without functional solutions.
-                                                    </p>
-                                                </div>
+
+                                                    
+                                                
                                                 </div>
                                             </div>
                                             </div>
                                         </div>
                                         </div>
-
-
-                                    <!-- Footer -->
-                                    <div id="errorMessage" class="hidden mt-1 bg-red-500 text-white p-1 rounded-b shadow-md items-center border-t border-solid border-slate-200">
-                                        <span>Error, prencha corretamente todos os campos</span>
-                                    </div>
-                                    <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                                        <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" onclick="closeModal('modal-id')">
-                                            Fechar
-                                        </button>
-                                        <button class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" onclick="updateUser(event)">
-                                            Salvar
-                                        </button>    
-                                    </div>
                                 </div>
                         </div>
                     </div>
