4b9308bfdaa3195464fee5e633ee077adc13cee8
Bruno <bettiol.2023@gmail.com>
2023-10-17 16:53:21 -0300

    progresso no modulo portaria

--- resources/views/ControleDeAcesso.blade.php
+++ resources/views/ControleDeAcesso.blade.php
@@ -14,10 +14,86 @@
 
 
 
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
+                    <button
+                        class="self-start bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 mb-4"
+                        type="button"
+                        onclick="toggleModal('modal-id-add', $(this).data('url'), $(this).data('referencia'))">
+                        Novo
+                    </button>
+
+                    <!-- Model Adicionar Encaixe -->
+                    <div class="hidden overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center"
+                        id="modal-id-add">
+                        <div class="relative w-2/5 h-1/3 my-6 mx-auto">
+                            <div
+                                class="border-0 rounded-lg shadow-lg relative flex flex-col w-full h-full bg-white outline-none focus:outline-none">
+
+                                <div
+                                    class="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                                    <h3 class="text-3xl font-semibold">
+                                        Adicionar Acesso
+                                    </h3>
+                                </div>
+
+                                <form id="formAddEncaixe" class="bg-white p-4 rounded-lg shadow-md">
+
+                                    <div class="mb-4">
+                                        <label for="referencia" class="block text-sm font-medium text-gray-700">
+                                            NOME
+                                        </label>
+                                        <input type="text" id="nome" name="nome"
+                                            class="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-emerald-400">
+                                    </div>
+
+                                    <div class="mb-4">
+                                        <label for="referencia" class="block text-sm font-medium text-gray-700">
+                                            RG ou CPF
+                                        </label>
+                                        <input type="text" id="referencia" name="referencia"
+                                            class="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-emerald-400">
+                                    </div>
+
+                                    <div class="mb-4">
+                                        <label for="referencia" class="block text-sm font-medium text-gray-700">
+                                            TRANSPORTADORA
+                                        </label>
+                                        <input type="text" id="referencia" name="referencia"
+                                            class="w-full border rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-emerald-400">
+                                    </div>
+
+                                    <!-- Error Message -->
+                                    <div id="errorMessage" class="bg-red-500 text-white p-2 rounded-md mb-4 hidden">
+                                        <span>Error, preencha corretamente todos os campos</span>
+                                    </div>
+
+                                    <!-- Buttons -->
+                                    <div class="flex justify-end space-x-3">
+                                        <button
+                                            class="text-red-500 font-bold text-sm outline-none hover:text-red-700 transition-colors duration-300"
+                                            type="button" onclick="closeModal('modal-id-add')">
+                                            Fechar
+                                        </button>
+                                        <button
+                                            class="bg-emerald-500 text-white font-bold text-sm py-2 px-4 rounded shadow hover:shadow-md transition duration-300"
+                                            onclick="adicionarEncaixe(event, document.getElementById('referencia').value, '{{ url('') }}')">
+                                            Salvar
+                                        </button>
+                                    </div>
+
+                                </form>
+
+
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hidden opacity-25 fixed inset-0 z-40 bg-black" id="modal-id-add-backdrop"></div>
+
                     <button
                         class="self-start bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 transition duration-300 mb-4"
                         type="button" onclick="toggleModal('modal-id-add')">
@@ -316,6 +392,92 @@
 
     });
 </script>
+
+
+<script src="http://localhost/javascript/controleDeAcesso.js" type="text/javascript"></script>
+
+<script type="module">
+    $(document).ready(function() {
+        $('#myTable').DataTable({
+            dom: 'Pfrtip',
+    buttons: [
+        'copy', 'excel', 'pdf'
+    ],
+    searchPanes:{
+        
+    },
+    
+            processing: true,
+            serverSide: true,
+            ajax: "controleDeAcesso",   
+            columnDefs: [{
+                    className: "align-left",
+                    "targets": 0,
+                    render: function(data) {
+                        return new Date(data).toLocaleDateString();
+                    }
+                },
+
+            ],
+            columns: [{
+                    data: 'created_at',
+                    orderable: true
+                }, {
+                    data: 'nome',
+                    name: 'nome'
+                },
+                {
+                    data: 'rgCpf',
+                    orderable: false
+                },
+                {
+                    data: 'empresa',
+                    orderable: false
+                },
+                {
+                    data: 'horaEntrada',
+                    orderable: true
+                },
+                {
+                    data: 'horaSaida.saida',
+                    orderable: true,
+                    render: function(data, type, row) {
+
+
+                        if (data == null) {
+                            return `<button onclick="registrarHoraSaida(${row.horaSaida.id})" class="bg-yellow-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+                                     Registrar
+                                    </button>`
+                        }
+
+                        return data
+                    }
+                },
+                {
+                    data: 'actions',
+                    orderable: false,
+                    render: function(data, type, row) {
+
+                        var actionsData = data
+
+                        return `
+                            <button class="bg-blue-500 text-white active:bg-pink-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                onclick="mostrarAcesso('${ actionsData.url_show }')">
+                               EDITAR
+                            </button>
+                            
+                        `;
+                    }
+                },
+            ]
+
+        });
+
+    });
+</script>
+
+
+<script src="http://localhost/javascript/controleDeAcesso.js" type="text/javascript"></script>
 
 
 <script src="http://localhost/javascript/controleDeAcesso.js" type="text/javascript"></script>
