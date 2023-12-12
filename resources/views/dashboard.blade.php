12a469cafb85ef95538a5f335132a580f3d20d17
bruno <bettiol.2023@gmail.com>
2023-12-12 15:41:05 -0300

    ADD: chaves to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -69,6 +69,21 @@
                         </a>
                     @endcan
 
+                    @can('chaves')
+                        <a href="{{ route('chaves') }}"
+                            class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg"
+                                src="img/chaves.png" alt="">
+                            <div class="flex flex-col justify-between p-4 leading-normal">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Módulo de
+                                    Chaves</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Simplifica o gerenciamento de
+                                    entrada e saída de chaves.
+                                </p>
+                            </div>
+                        </a>
+                    @endcan
+
                 </div>
             </div>
         </div>
